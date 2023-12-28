a2413eaec68b8e5c93c474f5a23a20b44fc52ca9
TAKAMURA Akihisa <tkmr.akhs@gmail.com>
2023-12-28 16:37:01 +0900

    Set-EventLogArchiveの実装
    基本的な実装を完了した。

--- Set-EventLogArchive.ps1
+++ Set-EventLogArchive.ps1
@@ -18,11 +18,72 @@ function Set-EventLogArchive {
     [CmdletBinding(SupportsShouldProcess)]
     Param (
         [Parameter(Mandatory = $true, Position = 0)]
-        [string]$Param1,
+        [string[]]$LogName,
 
-        [Parameter(Position = 1)]
-        [string]$Param2 = "DefaultValue"
+        [Parameter()]
+        [string[]]$Path,
+
+        [Parameter()]
+        [Nullable[int]]$Size,
+
+        [Parameter()]
+        [Nullable[bool]]$Retention,
+
+        [Parameter()]
+        [Nullable[bool]]$AutoBackup
     )
 
-    # 処理
+        #Write-Host "LogName: $($LogName)"
+        #Write-Host "LogName is null?: $($null -eq $LogName)"
+        #Write-Host "Path: $($Path)"
+        #Write-Host "Path is null?: $($null -eq $Path)"
+        #Write-Host "Size: $($Size)"
+        #Write-Host "Size is null?: $($null -eq $Size)"
+        #Write-Host "Retention: $($Retention)"
+        #Write-Host "Retention is null?: $($null -eq $Retention)"
+        #Write-Host "AutoBackup: $($AutoBackup)"
+        #Write-Host "AutoBackup is null?: $($null -eq $AutoBackup)"
+
+
+    if ($null -ne $Path -and $Path -ne "") {
+        $DirPath = [System.Environment]::ExpandEnvironmentVariables("$($Path)\..")
+        $FullDirPath = [System.IO.Path]::GetFullPath($DirPath)
+        if (Test-Path $FullDirPath) {
+            $pathOption = "/logfilename:$($Path)"
+        }
+        else {
+            Write-Error "'Path' is invalid."
+            return
+        }
+    }
+
+    if ($null -ne $Retention) {
+        $retentionStr = $Retention ? "true" : "false"
+        $retentionOption = "/retention:$($retentionStr)"
+    }
+
+    if ($null -ne $AutoBackup) {
+        $autoBackupStr = $AutoBackup ? "true" : "false"
+        $autoBackupOption = "/autobackup:$($autoBackupStr)"
+    }
+
+    if ($null -ne $Size) {
+        $maxSizeOption = "/maxsize:$($Size)"
+    }
+
+    if ($null -eq $pathOption -and $null -eq $retentionOption -and $null -eq $autoBackupOption -and $null -eq $maxSizeOption) {
+        Write-Warning "No change."
+        return
+    }
+    
+    foreach ($item in $LogName) {
+        if ($PSCmdlet.ShouldProcess("EventLog: $($LogName)", "Change setting of event log.")) {
+        $output = (WEVTUTIL "set-log" "$($item)" "/quiet:true" $pathOption $retentionOption $autoBackupOption $maxSizeOption 2>&1)
+        
+        if (-not $?) {
+            Write-Error "$($output)"
+        }else{
+            Write-Debug "$($output)"
+        }}
+    }
 }
